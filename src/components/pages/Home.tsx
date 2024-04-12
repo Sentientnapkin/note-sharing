05dcce15c53ab609e19a304e18d68789a200d93f
FluffyBunny235 <25jsabo@athenian.org>
2024-04-12 14:35:41 -0700

    editing home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,6 +3,9 @@ import {useNavigate} from "react-router-dom";
 import { logOut } from '../../firebase/firebaseFunctions';
 import { onAuthStateChanged, User} from "firebase/auth";
 import {auth} from '../../firebase/firebaseSetup';
+import { FormControl, Input, InputAdornment, InputLabel, TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
+import styles from '../../styles/home.module.css'
 
 
 export default function Home() {
@@ -25,10 +28,26 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Welcome to Athenian High School's Note Sharing Website</h1>
+      <div className={styles.title}>
+        <h1>Hadrian's Library</h1>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={classes}
+          sx={{ width: 900 }}
+          renderInput={(params) => <TextField {...params} label="Topic" />}
+        />
+      </div>
       <p>Here you can find notes from your classmates, or share your own notes with others.</p>
       <p>Click on the "Notes" tab to get started.</p>
       <button onClick={logOut}>Sign Out</button>
     </div>
   );
 }
+const classes = [
+  {label: 'Computer Science'},
+  {label: 'Advanced Computer Science'},
+  {label: 'Computational Thinking'},
+  {label: 'Data Structures & Algorithms'},
+  {label: 'Software Engineering'},
+];
