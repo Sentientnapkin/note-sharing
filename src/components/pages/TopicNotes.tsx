d56c0cc0a89b3e54db0349cd4d8b6685a7cd26d3
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2024-04-13 19:26:14 -0700

    Updated TopicId setting and Added prelim Uploading

--- src/components/pages/TopicNotes.tsx
+++ src/components/pages/TopicNotes.tsx
@@ -1,19 +1,58 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Input } from '@mui/material';
+import { storage, auth } from '../../firebase/firebaseSetup';
+import { ref, uploadBytes, list } from "firebase/storage";
+import { useParams } from 'react-router-dom';
 
 export default function TopicNotes() {
+  const { topicId } = useParams();
   const [file, setFile] = useState<File | null>(null)
 
   function handleUploadNote(event: React.ChangeEvent<HTMLInputElement>) {
     if (!event.target.files) return
     setFile(event.target.files[0])
-    console.log(event.target.files[0])
+    const storageRef = ref(storage, 'notes/' + topicId + '/' + event.target.files[0].name);
+    uploadBytes(storageRef, event.target.files[0]).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+    });
   }
+
+  async function getNotes() {
+    // get notes from firebase
+    const listRef = ref(storage, 'notes/' + topicId + '/');
+
+    // Fetch the first page of 100.
+    const firstPage = await list(listRef, {maxResults: 30});
+
+    // Use the result.
+    console.log(firstPage.items)
+    // processItems(firstPage.items)
+    // processPrefixes(firstPage.prefixes)
+
+    // Fetch the second page if there are more elements.
+    if (firstPage.nextPageToken) {
+      const secondPage = await list(listRef, {
+        maxResults: 100,
+        pageToken: firstPage.nextPageToken,
+      });
+      // processItems(secondPage.items)
+      // processPrefixes(secondPage.prefixes)
+    }
+  }
+
+  useEffect(() => {
+    getNotes().then(r => console.log(r))
+  }, [])
 
   return (
     <div>
       <h1>Topic Notes</h1>
-      <Input type="file" onChange={handleUploadNote} disableUnderline={true}/>
+      <h2> topicId: {topicId} </h2>
+      <Input type="file" onChange={handleUploadNote} disableUnderline={true} inputProps={{accept:"application/pdf"}}/>
+      <div>
+        <h2>Notes</h2>
+
+      </div>
     </div>
   );
 }
