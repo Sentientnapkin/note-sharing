aaf59a7f03bd69522bc91799d6e378f98fec2a8e
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2024-05-23 12:03:26 -0700

    Changed Way Notes and note data is uploaded

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,9 +2,10 @@ import React, {useCallback, useEffect, useRef, useState} from 'react';
 import BackButton from '../components/BackButton';
 import styles from '../styles/profile.module.css';
 import Button from '@mui/material/Button'
-import { TextField } from '@mui/material';
+import {CircularProgress, TextField} from '@mui/material';
 import {auth, db, storage} from '../firebase/firebaseSetup';
 import {getDownloadURL, getMetadata, list, listAll, ref, StorageReference} from "firebase/storage";
+import {collection, doc, getDocs, query} from "firebase/firestore";
 
 export default function Profile() {
   const [notes, setNotes] = useState<any[]>([])
@@ -13,74 +14,40 @@ export default function Profile() {
   async function getMyUploads() {
     setIsLoading(true)
 
-    const listRef1 = ref(storage, 'notes/');
+    const allNotes: { name: any; fullPath: any; unit: any; classDate: any; }[] = [];
+    const user = auth.currentUser;
+    if (user === null) return;
+    const uid = user.uid;
 
-    const directories = await listAll(listRef1)
-      .then(async (res) => {
-        let allNotes: StorageReference[] = [];
+    const q = query(collection(db, "users", uid, "uploads"));
+    const querySnapshot = await getDocs(q);
+    const docs = querySnapshot.docs.map(doc => doc.data());
+    docs.forEach((doc) => {
+      // get firebase storage url for each note from info from firestore
+      allNotes.push({name: doc.note_name, fullPath: doc.fullPath, unit: doc.unit, classDate: doc.classDate})
+    })
 
-        for (let i = 0; i < res.prefixes.length; i++) {
-          const subject = res.prefixes[i].name
-          const listRef2 = ref(storage, 'notes/' + subject + "/");
-
-          const subjectPage = await list(listRef2)
-            .then(async(res) => {
-              for (let i = 0; i < res.prefixes.length; i++) {
-                const classId = res.prefixes[i].name
-                const listRef3 = ref(storage, 'notes/' + subject + "/" + classId + "/");
-
-                const classsesPage = await list(listRef3, {maxResults: 100}).then(
-                  async (res) => {
-                    for (let i = 0; i < res.prefixes.length; i++) {
-                      const unit = res.prefixes[i]
-                      const listRef4 = ref(storage, 'notes/' + subject + "/" + classId + "/" + unit.name + "/");
-
-                      const unitPage = await list(listRef4, {maxResults: 100}).then(
-                        async (res) => {
-                          const firstPage = res
-                          for (let i = 0; i < firstPage.items.length; i++) {
-                            const note = firstPage.items[i]
-                            const metadata = await getMetadata(ref(storage, note.fullPath)).then((metadata) => {
-                              return metadata.customMetadata
-                            })
-                            if (metadata?.uploadedBy === auth.currentUser?.displayName) {
-                              allNotes.push(note)
-                            }
-                          }
-                        }
-                      )
-                    }
-                  }
-                )
-              }
-          })
-          setIsLoading(false)
-        }
-
-        const fullNotes = allNotes.map(async (note) => {
-          const metadata = await getMetadata(ref(storage, note.fullPath)).then((metadata) => {
-            return metadata.customMetadata
-          })
-          const name = note.name.substring(0, note.name.length - 4);
-          const classDate = metadata?.classDate
-          const unit = metadata?.unit
-          const uploadedBy = metadata?.uploadedBy
-          let url = "";
-          await getDownloadURL(ref(storage, note.fullPath)).then((downloadUrl) => {
-            if (downloadUrl === undefined) return;
-            url = downloadUrl;
-            console.log(downloadUrl)
-          }).catch((error) => {
-            console.error(error);
-          });
-          return {name: name, fileName: note.name, fullPath: note.fullPath, classDate: classDate, unit: unit, uploadedBy: uploadedBy, downloadUrl: url}
-        })
-
-        setNotes(await Promise.all(fullNotes))
-        console.log(fullNotes)
-
-        return res.prefixes
+    const fullNotes = allNotes.map(async (note) => {
+      const metadata = await getMetadata(ref(storage, note.fullPath)).then((metadata) => {
+        return metadata.customMetadata
       })
+      const name = metadata?.note_name;
+      const classDate = metadata?.classDate
+      const unit = metadata?.unit
+      const uploadedBy = metadata?.uploadedBy
+      let url = "";
+      await getDownloadURL(ref(storage, note.fullPath)).then((downloadUrl) => {
+        if (downloadUrl === undefined) return;
+        url = downloadUrl;
+        console.log(downloadUrl)
+      }).catch((error) => {
+        console.error(error);
+      });
+      return {name: name, fileName: note.name, fullPath: note.fullPath, classDate: classDate, unit: unit, uploadedBy: uploadedBy, downloadUrl: url}
+    })
+
+    setNotes(await Promise.all(fullNotes))
+    setIsLoading(false)
   }
 
   useEffect(() => {
@@ -93,10 +60,10 @@ export default function Profile() {
       <h1 className={styles.title}>Account Profile</h1>
       <p className={styles.para}>Username: {auth.currentUser?.displayName}</p>
       <h2>My Uploads</h2>
-      {isLoading && <p>Loading...</p>}
+      {isLoading && <CircularProgress color="inherit" />}
       {notes.map((note) => {
         return (
-          <div>
+          <div key={note.name}>
             <p>{note.name}</p>
             <p>{note.classDate}</p>
             <iframe src={note.downloadUrl} title={note.name}/>
